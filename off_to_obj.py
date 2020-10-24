e22ef6139e0a0f50b3c4c8de97ae7d67ecb76f6e
Leo Dong <leodong0315@gmail.com>
2020-10-23 17:27:19 -0700

    fix conv

--- off_to_obj.py
+++ off_to_obj.py
@@ -6,7 +6,7 @@ path = "/viscam/u/leozdong/occnet/demo/generation/vis"
 for i in range(9):
     input_name = "0{}_mesh.off".format(i)
     input_file = os.path.join(path, input_name)
-    output_name = "0{}_mesh.obj".formate(i)
+    output_name = "0{}_mesh.obj".format(i)
     output_file = os.path.join(path, output_name)
 
     mesh = pymesh.load_mesh(input_file)
