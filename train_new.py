588cd03be8c589b69824dfbabda730d88c8d0843
Ubuntu <soniacs229@OccupancyNetwork.4iq1xqlvbgeezne0uhwn3mllgf.ex.internal.cloudapp.net>
2020-11-18 03:27:46 +0000

    added evaluation code

--- train_new.py
+++ train_new.py
@@ -25,11 +25,11 @@ device = torch.device("cuda" if is_cuda else "cpu")
 model = DecoderOnlyModule(decoder.Decoder(c_dim=0), device=device)
 
 # Intialize training
-# shape_id = '7c13a71834d2b97687cc3b689b9b258d'
+shape_id = '7c13a71834d2b97687cc3b689b9b258d'
 # shape_id = '24d07a3c5ff0840872152988eac576ab'
 # shape_id = '36190ce6fe041e452d647b1c17442c93'  # does not have model
 # shape_id = '49c2f144b928726572a38ac2b8f5cd48'
-shape_id = '53737a4b45fc06963ffe0e5069bf1eb5'
+# shape_id = '53737a4b45fc06963ffe0e5069bf1eb5'
 
 
 
@@ -59,11 +59,15 @@ visualize.visualize_voxels_new(voxel_data.unsqueeze(0), 'input_voxel_mc', save_p
 
 
 # pointclouds
-# points_field = PointsField('points.npz')
-# points = points_field.load(os.path.join(data_dir, shape_id), 0, 0)
 pointcloud_field = PointCloudField('pointcloud.npz')
 pointcloud = pointcloud_field.load(os.path.join(data_dir, shape_id), 0, 0)[None]
 # import ipdb; ipdb.set_trace ()
+
+# points for validation
+points_field = PointsField('points.npz')
+points_dict = points_field.load(os.path.join(data_dir, shape_id), 0, 0)
+points = points_dict[None]
+points_occ = points_dict['occ']
 
 #
 # points_file = np.load(os.path.join(data_dir, shape_id, 'points.npz'))
@@ -98,8 +102,30 @@ def plot_loss(loss_rec):
         ax.plot(x[start:], y[start:])
         ax.set(xlabel='iteration', ylabel='loss',
                title='Loss record starting at 500 iterations')
-
+        
         fig.savefig(os.path.join(save_path, "loss.png"), dpi=1000)
+
+def plot_eval(entropy_rec, iou_rec):
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    x = np.arange(1, len(entropy_rec) + 1, 1)
+    y1 = entropy_rec
+    y2 = iou_rec
+    
+    fig_entropy, ax1 = plt.subplots()
+    fig_iou, ax2 = plt.subplots()
+    
+    ax1.plot(x, y1)
+    ax2.plot(x, y2)
+    ax1.set(xlabel='iteration', ylabel='eval',
+           title='entropy record starting at 0')
+    ax2.set(xlabel='iteration', ylabel='eval',
+           title='iou record starting at 0')
+
+    fig_entropy.savefig(os.path.join(save_path, "eval_entropy.png"), dpi=1000)
+    fig_iou.savefig(os.path.join(save_path, "eval_iou.png"), dpi=1000)
+
 
 # Configure training loop
 it = 0
@@ -107,15 +133,25 @@ print_every = 10
 vis_every = 1000
 plot_every = 5000
 loss_rec = []
+## for eval
+entropy_rec = []
+iou_rec = []
 while True:
     it += 1
     loss = trainer.train_step(voxel_data, n_points=10000)
-    loss_rec.append(loss)
+    loss_rec.append(loss.item())
+
+    ## for eval
+    eval_dict = trainer.eval_step(points, points_occ)
+    entropy_eval = eval_dict['cross_entropy']
+    iou_eval = eval_dict['iou']
+    entropy_rec.append(entropy_eval.item())
+    iou_rec.append(iou_eval.item())
 
     # Print output
     if print_every > 0 and (it % print_every) == 0:
-        print('it=%03d loss=%.4f'
-              % (it, loss))
+        print('it=%03d loss=%.4f entropy_eval=%0.4f iou_eval=%0.4f)'
+              % (it, loss, eval_dict['cross_entropy'], eval_dict['iou']))
 
     # Visualize shape
     if vis_every > 0 and (it == 1 or (it % vis_every) == 0):
@@ -125,5 +161,10 @@ while True:
 
     # Plot loss
     if plot_every > 0 and (it == 1 or (it % plot_every) == 0):
-        print("Plotting...")
+        print("Plotting loss...")
         plot_loss(loss_rec)
+
+        ## for eval
+        print("Plotting eval...")
+        plot_eval(entropy_rec, iou_rec)
+
